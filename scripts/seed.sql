b093d42be8a837350b2a0e30e3e321bc60223aa3
Jacob Freeman <t00niyf5@duck.com>
2025-11-02 18:34:24 -0500

    chore: FIx whitespace

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -1,12 +1,15 @@
 INSERT INTO users
 	("id", "email", "role")
 	VALUES (uuidv7(), 'admin__not_an_email', 'admin');
+
 INSERT INTO users
 	("id", "email", "role")
 	VALUES (uuidv7(), 'moderator__not_an_email', 'moderator');
+
 INSERT INTO users
 	("id", "email", "role")
 	VALUES (uuidv7(), 'user__not_an_email', 'user');
+
 
 INSERT INTO events
 	("title", "description", "date", "location", "zip_code", "council_id", "approved")
